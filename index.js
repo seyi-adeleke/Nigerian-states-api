4d23697a93c88862d5d143b5ca64e2b2dc979050
seyi adeleke <seyiadeleke42@yahoo.com>
2021-03-12 11:51:08 +0100

    update node version

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 /**
  * Created by seyi adeleke on 2/23/2017.
  */
+"use strict";
 var express = require('express');
-require('dotenv').config()
+require('dotenv').config();
 var app = express();
 
 var env = process.env.NODE_ENV =  process.env.NODE_ENV || "development";
