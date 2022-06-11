57ed78ea7f38266a72984cbbc35f683e23c64250
Seyi Adeleke <seyi.adeleke@paystack.com>
2022-06-11 11:34:29 +0100

    lint

--- index.js
+++ index.js
@@ -2,14 +2,13 @@
  * Created by seyi adeleke on 2/23/2017.
  */
 "use strict";
-var express = require('express');
-require('dotenv').config();
+var express = require("express");
+require("dotenv").config();
 var app = express();
 
-var env = process.env.NODE_ENV =  process.env.NODE_ENV || "development";
+var env = (process.env.NODE_ENV = process.env.NODE_ENV || "development");
 var config = require("./server/config")[env];
 
 require("./server/mongoose")(config);
-require("./server/express")(app,config);
+require("./server/express")(app, config);
 require("./server/apiRoutes")(app);
-
